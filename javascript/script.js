613091431b1414d88b987ca87924965e4a45952b
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2023-06-15 00:08:29 +0800

    fix: ui

--- javascript/script.js
+++ javascript/script.js
@@ -6,12 +6,12 @@ function insertProject(containerId, project) {
 	const projectContainer = $(containerId);
 	const projectCard = $("<div>", {
 		class:
-			"card rounded-lg shadow-sm border-none overflow-hidden p-5 m-4 flex flex-col animate__animated animate__fadeIn",
+			" border-none overflow-hidden p-5 m-4 flex flex-col animate__animated animate__fadeIn",
 	});
 
 	const contentContainer = $("<div>", { class: "px-5 py-2" });
 	const titleElement = $("<h3>", {
-		class: "text-xl font-bold mb-2  my-text-secondary-color",
+		class: "text-2xl font-bold mb-2  my-text-secondary-color",
 		text: project.title,
 	});
 	const descriptionElement = $("<p>", {
@@ -25,6 +25,7 @@ function insertProject(containerId, project) {
 	});
 
 	contentContainer.append(titleElement);
+	contentContainer.append(document.createElement("hr"));
 	contentContainer.append(languageElement);
 	contentContainer.append(descriptionElement);
 
@@ -33,7 +34,7 @@ function insertProject(containerId, project) {
 	project.topics.forEach((topic) => {
 		const topicElement = $("<span>", {
 			class:
-				"my-bg-secondary-color my-text-color shadow-sm text-xs bold rounded-full mr-2 px-2 py-1 m-2",
+				"my-bg-secondary-color my-text-color shadow text-xs bold rounded-full mr-2 px-2 py-1 m-2",
 			text: topic,
 		});
 		topicsContainer.append(topicElement);
@@ -76,7 +77,7 @@ function insertProject(containerId, project) {
 			const link = document.createElement("a");
 			link.setAttribute("href", imageUrl);
 			link.setAttribute("target", "_blank");
-			img.classList.add("h-auto", "max-w-full", "rounded-lg");
+			img.classList.add("h-auto", "max-w-full", "rounded-lg", "shadow");
 			img.setAttribute("src", imageUrl);
 			img.setAttribute("alt", "");
 			link.appendChild(img);
@@ -89,18 +90,6 @@ function insertProject(containerId, project) {
 
 	const imageGrid = createImageGrid(project.screenshots);
 
-	const videoContainer = $("<div>", {
-		class: "flex justify-center w-full m-2",
-	});
-	const videoElement = $("<iframe>", {
-		class: "m-2 inline rounded",
-		src: "https://www.youtube.com/embed/vCzhn-pHvGg",
-		text: "Repository",
-		allowfullscreen: true,
-	});
-
-	videoContainer.append(videoElement);
-
 	otherContainer.append(repoElement);
 	otherContainer.append(docElement);
 
@@ -110,7 +99,7 @@ function insertProject(containerId, project) {
 	projectCard.append(imageGrid);
 	let topicsTitle = document.createElement("h5");
 	topicsTitle.textContent = "Topics and keywords";
-	topicsTitle.classList.add("my-3", "my-text-secondary-color");
+	topicsTitle.classList.add("my-text-secondary-color");
 	projectCard.append(topicsTitle);
 	projectCard.append(topicsContainer);
 	projectContainer.append(projectCard);
