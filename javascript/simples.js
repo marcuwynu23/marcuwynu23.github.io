bbd80a050c7c3bd34cebcb979005822b3a8dcaf9
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2023-06-15 00:21:06 +0800

    update: fix ui

--- javascript/simples.js
+++ javascript/simples.js
@@ -1,7 +1,8 @@
 const simpleProjects = [
 	{
-		title: "Mushroom Webshop Website",
-		description: "The Kalapatid Pigeon Raising and Sport Management System is a web-based application built using JavaScript that manages the pigeon raising and sport activities of the Kalapatid Pigeon Club. It enables members to manage their pigeon inventory, track breeding activities, register for racing events, and communicate with other members.",
+		title: "Mushroom Shop Website",
+		description:
+			"With a user-friendly interface, it offers a diverse range of fresh, dried, and powdered mushrooms sourced from trusted suppliers. Explore an extensive selection of mushroom products, including supplements, tinctures, and skincare items. Dive into a wealth of educational resources and expert advice to expand your knowledge and appreciation for mushrooms. Engage with a passionate community of mushroom enthusiasts, share experiences, and discover new culinary adventures. Experience the Mushroom Shop Website and embark on a journey into the fascinating realm of fungi.",
 		link: "https://github.com/marcuwynu23/mushroom-website.git",
 		language: "Flask and Jinja Template Engine",
 		topics: ["pigeon-raising", "sport", "agriculture", "nodejs"],
@@ -10,42 +11,63 @@ const simpleProjects = [
 			"assets/screenshots/simples/mushroom/1.jpg",
 			"assets/screenshots/simples/mushroom/0.jpg",
 			"assets/screenshots/simples/mushroom/1.jpg",
-		]
+		],
 	},
 	{
 		title: "Dummy Text Generator",
-		description: "A Browser Extension help generate random words, sentences, or paragraph",
+		description:
+			"A Browser Extension help generate random words, sentences, or paragraph",
 		link: "https://github.com/marcuwynu23/Dummy-Text-Generator.git",
 		language: "Javascript",
-		topics: ["browser-extension", "chrome-extension", "firefox-extension", "dummy-text", "text-generator", "random-text", "random-words", "random-sentences", "random-paragraph"],
+		topics: [
+			"browser-extension",
+			"chrome-extension",
+			"firefox-extension",
+			"dummy-text",
+			"text-generator",
+			"random-text",
+			"random-words",
+			"random-sentences",
+			"random-paragraph",
+		],
 		screenshots: [
 			"assets/screenshots/simples/dummy-text-generator/0.jpg",
 			"assets/screenshots/simples/dummy-text-generator/0.jpg",
 			"assets/screenshots/simples/dummy-text-generator/0.jpg",
 			"assets/screenshots/simples/dummy-text-generator/0.jpg",
-		]
-
+		],
 	},
 	{
 		title: "Web QR Code Generator",
 		description: "A Sample QR Code Generator Web Application",
 		link: "https://github.com/marcuwynu23/qr-code-generator.gits",
 		language: "Javascript",
-		topics: ["node.js", "qr", "qr-code", "qr-code-generator", "qr-code-generator-web", "qr-code-generator-web-application", "qr-code-generator-web-app"],
+		topics: [
+			"node.js",
+			"qr",
+			"qr-code",
+			"qr-code-generator",
+			"qr-code-generator-web",
+			"qr-code-generator-web-application",
+			"qr-code-generator-web-app",
+		],
 		screenshots: [
 			"assets/screenshots/simples/qr-generator/0.jpg",
 			"assets/screenshots/simples/qr-generator/0.jpg",
 			"assets/screenshots/simples/qr-generator/0.jpg",
 			"assets/screenshots/simples/qr-generator/0.jpg",
-		]
-	}, {
+		],
+	},
+	{
 		title: "Quiz App",
 		description: "A Sample Quiz App",
 		link: "https://github.com/marcuwynu23/quiz-app.git",
 		language: "Javascript, HTML, CSS,Angular and Bootstrap",
 		topics: ["quiz", "quiz-app", "quiz-app-web", "angular", "angular-quiz-app"],
-		screenshots: []
-	}
-]
+		screenshots: [],
+	},
+];
 let simpleProjectContainerId = "#simple-programs-container";
-simpleProjects.forEach(project => insertProject(simpleProjectContainerId, project))
+simpleProjects.forEach((project) =>
+	insertProject(simpleProjectContainerId, project)
+);
