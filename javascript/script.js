5355dba098ef48cc8e2bc4b895d892d1e007d0cc
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2023-06-26 20:38:53 +0800

    .

--- javascript/script.js
+++ javascript/script.js
@@ -1,161 +1,169 @@
 $("#toggleBtn").on("click", function () {
-	$("#menu").toggleClass("hidden");
+  $("#menu").toggleClass("hidden");
 });
 
 function insertProject(containerId, project) {
-	const projectContainer = $(containerId);
-	const projectCard = $("<div>", {
-		class:
-			"card overflow-hidden p-5 m-4 flex flex-col animate__animated animate__fadeIn",
-	});
+  const projectContainer = $(containerId);
+  const projectCard = $("<div>", {
+    class:
+      "card overflow-hidden p-5 m-4 flex flex-col animate__animated animate__fadeIn",
+  });
 
-	const contentContainer = $("<div>", { class: "px-5 py-2" });
-	const titleElement = $("<h3>", {
-		class: "text-2xl font-bold mb-2",
-		text: project.title,
-	});
-	const descriptionElement = $("<p>", {
-		class: " text-gray-200 text-justify ",
-		text: project.description,
-	});
+  const contentContainer = $("<div>", { class: "px-5 py-2" });
+  const titleElement = $("<h3>", {
+    class: "text-2xl font-bold mb-2",
+    text: project.title,
+  });
+  const descriptionElement = $("<p>", {
+    class: " text-gray-200 text-justify ",
+    text: project.description,
+  });
 
-	const languageElement = $("<span>", {
-		class: "text-sm text-gray-300 py-5",
-		text: project.language,
-	});
+  const languageElement = $("<span>", {
+    class: "text-sm text-gray-300 py-5",
+    text: project.language,
+  });
 
-	contentContainer.append(titleElement);
-	contentContainer.append(languageElement);
-	contentContainer.append(descriptionElement);
+  contentContainer.append(titleElement);
+  contentContainer.append(languageElement);
+  contentContainer.append(descriptionElement);
 
-	const otherContainer = $("<div>", {
-		class: "flex flex-wrap m-2 text-center text-sm",
-	});
+  const otherContainer = $("<div>", {
+    class: "flex flex-wrap m-2 text-center text-sm",
+  });
 
-	const repoElement = $("<a>", {
-		class: " p-2 m-2  inline border rounded text-xs text-gray-100 ",
-		href: project.link,
-		text: "Repository",
-	});
+  const repoElement = $("<a>", {
+    class: " p-2 m-2  inline border  text-xs text-gray-100 ",
+    href: project.link,
+    text: "Repository",
+  });
 
-	const docElement = $("<a>", {
-		class: "border p-2 m-2  inline  rounded text-xs text-gray-100 ",
-		href: project.link,
-		text: "Documentation",
-	});
+  let repoIcon = document.createElement("i");
+  repoIcon.className = "fas fa-link mr-2";
+  repoElement.prepend(repoIcon);
 
-	function createImageGallery(images) {
-		const container = document.createElement("div");
-		container.classList.add("w-full", "max-w-lg", "mx-auto");
+  const docElement = $("<a>", {
+    class: "border p-2 m-2  inline   text-xs text-gray-100 ",
+    href: project.link,
+    text: "Documentation",
+  });
 
-		const previewContainer = document.createElement("div");
-		previewContainer.classList.add("relative", "overflow-hidden", "h-64");
+  let docIcon = document.createElement("i");
+  docIcon.className = "fas fa-book-open mr-2";
+  docElement.prepend(docIcon);
 
-		const previewImage = document.createElement("img");
-		previewImage.classList.add(
-			"object-cover",
-			"h-full",
-			"w-full",
-			"border",
-			"border-gray-700",
-			"rounded"
-		);
-		previewImage.setAttribute("src", images[0] || "../assets/placeholder.jpg");
-		previewImage.setAttribute("alt", "Preview Image");
+  function createImageGallery(images) {
+    const container = document.createElement("div");
+    container.classList.add("w-full", "max-w-lg", "mx-auto");
 
-		previewContainer.appendChild(previewImage);
+    const previewContainer = document.createElement("div");
+    previewContainer.classList.add("relative", "overflow-hidden", "h-64");
 
-		// Create a preview button
-		const previewButton = document.createElement("button");
-		previewButton.classList.add(
-			"absolute",
-			"top-0",
-			"right-0",
-			"p-2",
-			"bg-gray-700",
-			"text-white",
-			"text-xs"
-		);
-		previewButton.textContent = "Preview";
-		previewButton.addEventListener("click", () => {
-			// Open a new window or popup to show the full image
-			window.open(images[0] || "../assets/placeholder.jpg", "_blank");
-		});
-		previewContainer.appendChild(previewButton);
+    const previewImage = document.createElement("img");
+    previewImage.classList.add(
+      "object-cover",
+      "h-full",
+      "w-full",
+      "border",
+      "border-gray-700"
+    );
+    previewImage.setAttribute("src", images[0] || "../assets/placeholder.jpg");
+    previewImage.setAttribute("alt", "Preview Image");
 
-		const slideContainer = document.createElement("div");
-		slideContainer.classList.add(
-			"flex",
-			"space-x-2",
-			"overflow-x-auto",
-			"mt-4",
-			"justify-center"
-		);
+    previewContainer.appendChild(previewImage);
 
-		images.forEach((imageUrl, index) => {
-			const slideImage = document.createElement("img");
-			slideImage.classList.add(
-				"object-cover",
-				"h-16",
-				"w-16",
-				"cursor-pointer",
-				"shadow-xl",
-				"border",
-				"border-gray-700",
-				"rounded"
-			);
-			slideImage.setAttribute("src", imageUrl || "../assets/placeholder.jpg");
-			slideImage.setAttribute("alt", `Slide Image ${index + 1}`);
+    // Create a preview button
+    const previewButton = document.createElement("button");
+    previewButton.classList.add(
+      "absolute",
+      "top-0",
+      "right-0",
+      "p-2",
+      "bg-transparent",
+      "text-white",
+      "text-xs"
+    );
+    let icon = document.createElement("i");
+    icon.classList.add("fas", "fa-eye");
+    previewButton.appendChild(icon);
+    previewButton.addEventListener("click", () => {
+      // Open a new window or popup to show the full image
+      window.open(images[0] || "../assets/placeholder.jpg", "_blank");
+    });
+    previewContainer.appendChild(previewButton);
 
-			slideImage.addEventListener("click", () => {
-				previewImage.setAttribute(
-					"src",
-					imageUrl || "../assets/placeholder.jpg"
-				);
+    const slideContainer = document.createElement("div");
+    slideContainer.classList.add(
+      "flex",
+      "space-x-2",
+      "overflow-x-auto",
+      "mt-4",
+      "justify-center"
+    );
 
-				// Remove the highlight class from all slide images
-				const slideImages = slideContainer.querySelectorAll("img");
-				slideImages.forEach((img) => {
-					img.classList.remove("border-red-500");
-				});
+    images.forEach((imageUrl, index) => {
+      const slideImage = document.createElement("img");
+      slideImage.classList.add(
+        "object-cover",
+        "h-16",
+        "w-16",
+        "cursor-pointer",
+        "shadow-xl",
+        "border",
+        "border-gray-700"
+      );
+      slideImage.setAttribute("src", imageUrl || "../assets/placeholder.jpg");
+      slideImage.setAttribute("alt", `Slide Image ${index + 1}`);
 
-				// Add the highlight class to the selected slide image
-				slideImage.classList.add("border-red-500");
-			});
+      slideImage.addEventListener("click", () => {
+        previewImage.setAttribute(
+          "src",
+          imageUrl || "../assets/placeholder.jpg"
+        );
 
-			slideContainer.appendChild(slideImage);
-		});
+        // Remove the highlight class from all slide images
+        const slideImages = slideContainer.querySelectorAll("img");
+        slideImages.forEach((img) => {
+          img.classList.remove("border-red-500");
+        });
 
-		container.appendChild(previewContainer);
-		container.appendChild(slideContainer);
+        // Add the highlight class to the selected slide image
+        slideImage.classList.add("border-red-500");
+      });
 
-		return container;
-	}
+      slideContainer.appendChild(slideImage);
+    });
 
-	otherContainer.append(repoElement);
-	otherContainer.append(docElement);
+    container.appendChild(previewContainer);
+    container.appendChild(slideContainer);
 
-	projectCard.append(contentContainer);
+    return container;
+  }
 
-	projectCard.append(otherContainer);
+  otherContainer.append(repoElement);
+  otherContainer.append(docElement);
 
-	projectCard.append(createImageGallery(project.screenshots));
+  projectCard.append(contentContainer);
 
-	let topicsTitle = document.createElement("h5");
-	topicsTitle.textContent = "Topics and keywords";
-	topicsTitle.className = "text-lg font-bold text-gray-300 py-5";
-	topicsTitle.classList.add("my-text-secondary-color");
+  projectCard.append(otherContainer);
 
-	const topicsContainer = $("<div>", { class: "flex flex-wrap mt-2" });
-	project.topics.forEach((topic) => {
-		const topicElement = $("<span>", {
-			class: "topic-item border rounded  text-xs   mr-2 px-2 py-1 m-2",
-			text: topic,
-		});
-		topicsContainer.append(topicElement);
-	});
+  projectCard.append(createImageGallery(project.screenshots));
 
-	projectCard.append(topicsTitle);
-	projectCard.append(topicsContainer);
-	projectContainer.append(projectCard);
+  let topicsTitle = document.createElement("h5");
+  topicsTitle.textContent = "Topics and keywords";
+  topicsTitle.className = "text-lg font-bold text-gray-300 py-5";
+  topicsTitle.classList.add("my-text-secondary-color");
+
+  const topicsContainer = $("<div>", { class: "flex flex-wrap mt-2" });
+  project.topics.forEach((topic) => {
+    const topicElement = $("<span>", {
+      class: "topic-item border  text-xs   mr-2 px-2 py-1 m-2",
+      text: topic,
+    });
+    topicsContainer.append(topicElement);
+  });
+
+  projectCard.append(topicsTitle);
+  projectCard.append(topicsContainer);
+  projectContainer.append(projectCard);
 }
